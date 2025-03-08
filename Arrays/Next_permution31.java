be95a57e3b71baf7862365384d4ab99cc37c76c2
Sharath91130 <sharathkurer8055@gmail.com>
2025-03-08 19:04:54 +0530

    next permuation is done

--- Arrays/Next_permution31.java
+++ Arrays/Next_permution31.java
@@ -1,7 +1,29 @@
 package Arrays;
 
+import java.util.Arrays;
+
 class Next_permution31 {
-    public void nextPermutation(int[] nums) {
+    public  static void nextPermutation(int[] nums) {
+        int index=-1;
+
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums,0, nums.length-1);
+            return;
+        }
+
+        for(int j=nums.length-1;j>index;j--){
+            if(nums[j]>nums[index]){
+                swap(nums,index,j);
+            }
+        }
+        reverse(nums,index+1,nums.length-1);
+
         
     }
    public static void swap(int [] arr ,int left ,int right){
@@ -15,9 +37,15 @@ class Next_permution31 {
             return;
         }
         while(start<end){
-            arr[start]=arr[end];
+            swap(arr, start, end);
             start++;
             end--;
         }
+    }
+
+    public static void main(String[] args) {
+        int [] arr={3,2,1};
+        nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
